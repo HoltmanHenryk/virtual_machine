677aff9f949557e8cdb24929ee644f7aa78914c2
LeaoMartelo <leaomartelo@gmail.com>
2026-02-21 11:19:48 -0300

    example01 -> example02 and new example01

--- example_programs/example_01.asm
+++ example_programs/example_01.asm
@@ -1,34 +1,12 @@
-mov -5, %1
-mov 0, %4
-no_op
-ld %1, %2
-no_op
-sto_pc %0
-inc %3
-cmp %3, %4
-je %0
-no_op
-dec %3
-no_op
-sto_pc %9
-inc %1
-cmp %1, %4
-jne %9
-no_op
-mov 22, %6
-mov 20, %7
-sto_pc %9
-dec %6
-cmp %6, %7
-jge %9
-no_op
-mov 18, %6
-mov 20, %7
-sto_pc %9
-inc %6
-cmp %6, %7
-jle %9
-no_op
+mov 5, %1
+mov 10, %2
+mov 15, %3
+mov 20, %4
+mov 25, %5
+mov 30, %6
+mov 35, %7
+mov 40, %8
+mov 45, %9
+mov 50, %10
 state_dump
-program_dump
 halt
