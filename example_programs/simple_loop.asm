d3a039565f54d0c14536fd9fdfd14516337cd0f7
LeaoMartelo <leaomartelo@gmail.com>
2026-02-22 23:40:41 -0300

    implement labels to assembler make it a 2 pass assembler add a symbol table change jmp instructions arguments to be a literal value that gets replaced with the value of the label at assemble time didnt want to bump to gnu11 for not vendor locking this to gcc (ik clang and msvc support it but eh), so temporarely bumped it to c23 updated simple_loop.asm example to test new changes (compiles but not able to be run yet)

--- example_programs/simple_loop.asm
+++ example_programs/simple_loop.asm
@@ -1,11 +1,8 @@
-# store value 10 for comparasion
-mov 10, $10
-# store a "jump point" right before we increment
-sto_pc $0
+# Simple loop example
+mov 0, $1
+mov 10, $2
+start:
 inc $1
-# compare if our value is less then 10
-cmp $1, $10
-# jump to our stored point if is less or equal to 10
-jle $0
-state_dump
+cmp $1, $2
+jne .start
 halt
