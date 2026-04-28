f319945ed625c7d365ad1ee2b38c76c55ef3f884
HoltmanHenryk <185836570+HoltmanHenryk@users.noreply.github.com>
2026-04-28 10:02:46 -0300

    update example of lib loading to showcase the extern_str functionality

--- example_programs/externlib/secondlib.c
+++ example_programs/externlib/secondlib.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <stdint.h>
+#include "../../VMASM.h"
 
-typedef int32_t i32;
-
-i32 hello_another_lib(void) {
-    printf("Hello, from a second library!\n");
+WORD hello_another_lib(void) {
+    printf("Hello, %s, this is from a second library!\n", vmasm_get_globalstring());
     return 0;
 }
